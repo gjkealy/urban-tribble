9fa4e8beb43a30797be170cf188d7ac978c9e24b
gjkealy <gkealy108@gmail.com>
2016-04-01 10:22:52 -0500

    add header template and logo for contact page

--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,4 @@
-<header class="banner navbar navbar-default navbar-static-top" role="banner">
+<header class="banner navbar navbar-default navbar-fixed-top" role="banner">
   <div class="container">
     <div class="navbar-header">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
+      <a class="navbar-brand" href="<?= esc_url(home_url('/')); ?>"><img class="aneca-header" src="<?= get_template_directory_uri(); ?>/dist/images/aneca-short-red.svg"></a>
     </div>
 
     <nav class="collapse navbar-collapse" role="navigation">
