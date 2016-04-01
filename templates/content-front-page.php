3a561adc09a9d72af75472e2afc8e20bb3955e94
gjkealy <gkealy108@gmail.com>
2016-04-01 09:34:36 -0500

    add front page section template and styles

---
+++ templates/content-front-page.php
@@ -0,0 +1,44 @@
+<section class="front-page-section front-page-section-responsive">
+	<div class="row">
+		<div class="col-md-6 front-page-section-title">
+			<div class="front-page-section-title-content">
+				<h1><?php the_field('section_1_heading') ?></h1>
+			</div>
+		</div>
+		<div class="col-md-6 front-page-section-text">
+			<div class="front-page-section-text-content">
+				<p><?php the_field('section_1_text') ?></p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="front-page-section front-page-section-speed">
+	<div class="row">
+		<div class="col-md-6 front-page-section-text">
+			<div class="front-page-section-text-content left">
+				<p><?php the_field('section_2_text') ?></p>
+			</div>
+		</div>
+		<div class="col-md-6 front-page-section-title">
+			<div class="front-page-section-title-content">
+				<h1><?php the_field('section_2_heading') ?></h1>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="front-page-section front-page-section-seo">
+	<div class="row">
+		<div class="col-md-6 front-page-section-title">
+			<div class="front-page-section-title-content">
+				<h1><?php the_field('section_3_heading') ?></h1>
+			</div>
+		</div>
+		<div class="col-md-6 front-page-section-text">
+			<div class="front-page-section-text-content">
+				<p><?php the_field('section_3_text') ?></p>
+			</div>
+		</div>
+	</div>
+</section>
