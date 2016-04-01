f0075654b54d89c2e4032754295788f47e87f660
gjkealy <gkealy108@gmail.com>
2016-04-01 11:12:32 -0500

    remove collapse menu from header and include front page sections for small screens

--- templates/content-front-page.php
+++ templates/content-front-page.php
@@ -1,44 +1,91 @@
-<section class="front-page-section front-page-section-responsive">
-	<div class="row">
-		<div class="col-md-6 front-page-section-title">
-			<div class="front-page-section-title-content">
-				<h1><?php the_field('section_1_heading') ?></h1>
+<div class="hidden-md hidden-lg">
+	<section class="front-page-section-sm front-page-section-responsive">
+		<div class="row">
+			<div class="front-page-section-title-sm">
+				<div class="front-page-section-title-content-sm">
+					<h1><?php the_field('section_1_heading') ?></h1>
+				</div>
+			</div>
+			<div class="front-page-section-text-sm">
+				<div class="front-page-section-text-content-sm">
+					<p><?php the_field('section_1_text') ?></p>
+				</div>
 			</div>
 		</div>
-		<div class="col-md-6 front-page-section-text">
-			<div class="front-page-section-text-content">
-				<p><?php the_field('section_1_text') ?></p>
+	</section>
+	<section class="front-page-section-sm front-page-section-responsive">
+		<div class="row">
+			<div class="front-page-section-title-sm">
+				<div class="front-page-section-title-content-sm">
+					<h1><?php the_field('section_2_heading') ?></h1>
+				</div>
+			</div>
+			<div class="front-page-section-text-sm">
+				<div class="front-page-section-text-content-sm">
+					<p><?php the_field('section_2_text') ?></p>
+				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
+	<section class="front-page-section-sm front-page-section-responsive">
+		<div class="row">
+			<div class="front-page-section-title-sm">
+				<div class="front-page-section-title-content-sm">
+					<h1><?php the_field('section_3_heading') ?></h1>
+				</div>
+			</div>
+			<div class="front-page-section-text-sm">
+				<div class="front-page-section-text-content-sm">
+					<p><?php the_field('section_3_text') ?></p>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
 
-<section class="front-page-section front-page-section-speed">
-	<div class="row">
-		<div class="col-md-6 front-page-section-text">
-			<div class="front-page-section-text-content left">
-				<p><?php the_field('section_2_text') ?></p>
+<div class="hidden-sm hidden-xs">
+	<section class="front-page-section front-page-section-responsive">
+		<div class="row">
+			<div class="col-md-6 front-page-section-title">
+				<div class="front-page-section-title-content">
+					<h1><?php the_field('section_1_heading') ?></h1>
+				</div>
+			</div>
+			<div class="col-md-6 front-page-section-text">
+				<div class="front-page-section-text-content">
+					<p><?php the_field('section_1_text') ?></p>
+				</div>
 			</div>
 		</div>
-		<div class="col-md-6 front-page-section-title">
-			<div class="front-page-section-title-content">
-				<h1><?php the_field('section_2_heading') ?></h1>
-			</div>
-		</div>
-	</div>
-</section>
+	</section>
 
-<section class="front-page-section front-page-section-seo">
-	<div class="row">
-		<div class="col-md-6 front-page-section-title">
-			<div class="front-page-section-title-content">
-				<h1><?php the_field('section_3_heading') ?></h1>
+	<section class="front-page-section front-page-section-speed">
+		<div class="row">
+			<div class="col-md-6 front-page-section-text">
+				<div class="front-page-section-text-content left">
+					<p><?php the_field('section_2_text') ?></p>
+				</div>
+			</div>
+			<div class="col-md-6 front-page-section-title">
+				<div class="front-page-section-title-content">
+					<h1><?php the_field('section_2_heading') ?></h1>
+				</div>
 			</div>
 		</div>
-		<div class="col-md-6 front-page-section-text">
-			<div class="front-page-section-text-content">
-				<p><?php the_field('section_3_text') ?></p>
+	</section>
+
+	<section class="front-page-section front-page-section-seo">
+		<div class="row">
+			<div class="col-md-6 front-page-section-title">
+				<div class="front-page-section-title-content">
+					<h1><?php the_field('section_3_heading') ?></h1>
+				</div>
+			</div>
+			<div class="col-md-6 front-page-section-text">
+				<div class="front-page-section-text-content">
+					<p><?php the_field('section_3_text') ?></p>
+				</div>
 			</div>
 		</div>
-	</div>
-</section>
+	</section>
+</div>
